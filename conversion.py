0b885a0d444dcf1846e511ae330125fe9f8b0d6f
nicholas_degryse <ndegrys@wgu.edu>
2025-04-05 03:46:50 -0500

    D: created a list of numbers for each character in user name

--- conversion.py
+++ conversion.py
@@ -2,3 +2,12 @@ def name_to_lower(usr_name):
     lower_name = usr_name.lower()
     print("Made it to name_to_lower function")
     return lower_name
+
+def characters_to_numbers(usr_name):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    number_name = []
+    characters = list(usr_name)
+    for character in characters:
+        number = alphabet.index(character) + 1
+        number_name.append(number)
+    return number_name
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from conversion import name_to_lower
+from conversion import characters_to_numbers
+
 
 def main():
     usr_name = input("Hello and Welcome! Please enter your name: ")
     print(f"Pleased to meet you {usr_name}! Please standby while we convert your name.")
     usr_lower_name = name_to_lower(usr_name)
     print(f"Testing to lower: {usr_lower_name}")
+    usr_num_name = characters_to_numbers(usr_lower_name)
+    print(f"Your name according to the positions of the characters in the alphbet is {usr_num_name}")
 
 if __name__ == "__main__":
     main()
