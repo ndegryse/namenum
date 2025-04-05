a1416564a77b447368c0d14b9903cb8376c1f8ae
nicholas_degryse <ndegrys@wgu.edu>
2025-04-05 03:35:05 -0500

    C: added conversion to lowercase name

---
+++ conversion.py
@@ -0,0 +1,4 @@
+def name_to_lower(usr_name):
+    lower_name = usr_name.lower()
+    print("Made it to name_to_lower function")
+    return lower_name
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from conversion import name_to_lower
+
 def main():
-    print("Hello World")
+    usr_name = input("Hello and Welcome! Please enter your name: ")
+    print(f"Pleased to meet you {usr_name}! Please standby while we convert your name.")
+    usr_lower_name = name_to_lower(usr_name)
+    print(f"Testing to lower: {usr_lower_name}")
 
 if __name__ == "__main__":
     main()
